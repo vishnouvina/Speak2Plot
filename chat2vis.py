f09f72ee8b4ca60eeb75f0cbe4d3e2da57ee708f
Vishnou Vinayagame <vishnou.vinayagame@student-cs.fr>
2023-11-26 14:40:43 -0500

    first commit

---
+++ chat2vis.py
@@ -0,0 +1,127 @@
+#################################################################################
+# Chat2VIS 
+# https://chat2vis.streamlit.app/
+# Paula Maddigan
+#################################################################################
+
+import pandas as pd
+import openai
+import streamlit as st
+#import streamlit_nested_layout
+from classes import get_primer,format_question,run_request
+import warnings
+warnings.filterwarnings("ignore")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_page_config(layout="wide",page_title="Chat2VIS")
+
+st.markdown("<h2 style='text-align: center;padding-top: 0rem;'>Creating Visualisations using Natural Language with Code Llama</h2>", unsafe_allow_html=True)
+
+available_models = {"Code Llama":"CodeLlama-34b-Instruct-hf"}
+
+# List to hold datasets
+if "datasets" not in st.session_state:
+    datasets = {}
+    # Preload datasets
+    datasets["History"] = pd.read_csv("history.csv")
+    datasets["Feedbacks"] = pd.read_csv("translated_feedbacks.csv")
+    st.session_state["datasets"] = datasets 
+else:
+    # use the list already loaded
+    datasets = st.session_state["datasets"]
+
+#key_col1,key_col2 = st.columns(2)
+#hf_key = key_col1.text_input(label = ":hugging_face: HuggingFace Key:",help="Required for Code Llama", type="password")
+
+hf_key = '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'
+
+with st.sidebar:
+    # First we want to choose the dataset, but we will fill it with choices once we've loaded one
+    dataset_container = st.empty()
+
+    # Add facility to upload a dataset
+    try:
+        uploaded_file = st.file_uploader(":computer: Load a CSV file:", type="csv")
+        index_no=0
+        if uploaded_file:
+            # Read in the data, add it to the list of available datasets. Give it a nice name.
+            file_name = uploaded_file.name[:-4].capitalize()
+            datasets[file_name] = pd.read_csv(uploaded_file)
+            # We want to default the radio button to the newly added dataset
+            index_no = len(datasets)-1
+    except Exception as e:
+        st.error("File failed to load. Please select a valid CSV file.")
+        print("File failed to load.\n" + str(e))
+    # Radio buttons for dataset choice
+    chosen_dataset = dataset_container.radio(":bar_chart: Choose your data:",datasets.keys(),index=index_no)#,horizontal=True,)
+
+    # Check boxes for model choice
+    st.write(":brain: Choose your model(s):")
+    # Keep a dictionary of whether models are selected or not
+    use_model = {}
+    for model_desc,model_name in available_models.items():
+        label = f"{model_desc} ({model_name})"
+        key = f"key_{model_desc}"
+        use_model[model_desc] = st.checkbox(label,value=True,key=key)
+ 
+ # Text area for query
+question = st.text_area("What would you like to visualise?",height=10)
+go_btn = st.button("Go...")
+
+# Make a list of the models which have been selected
+selected_models = [model_name for model_name, choose_model in use_model.items() if choose_model]
+model_count = len(selected_models)
+
+# Execute chatbot query
+if go_btn and model_count > 0:
+    api_keys_entered = True
+    # Check API keys are entered.
+    if "Code Llama" in selected_models:
+        if not hf_key.startswith('hf_'):
+            st.error("Please enter a valid HuggingFace API key.")
+            api_keys_entered = False
+    if api_keys_entered:
+        # Place for plots depending on how many models
+        plots = st.columns(model_count)
+        # Get the primer for this dataset
+        primer1,primer2 = get_primer(datasets[chosen_dataset],'datasets["'+ chosen_dataset + '"]') 
+        # Create model, run the request and print the results
+        for plot_num, model_type in enumerate(selected_models):
+            with plots[plot_num]:
+                st.subheader(model_type)
+                try:
+                    # Format the question 
+                    question_to_ask = format_question(primer1, primer2, question, model_type)   
+                    # Run the question
+                    answer=""
+                    answer = run_request(question_to_ask, available_models[model_type], alt_key=hf_key)
+                    # the answer is the completed Python script so add to the beginning of the script to it.
+                    answer = primer2 + answer
+                    print("Model: " + model_type)
+                    print(answer)
+                    #plot_area = st.empty()
+                    #plot_area.pyplot(exec(answer))
+                    exec(answer)           
+                except Exception as e:
+                    print(e)
+                    st.error("Unfortunately the code generated from the model contained errors and was unable to execute.")
+
+# Display the datasets in a list of tabs
+# Create the tabs
+tab_list = st.tabs(datasets.keys())
+
+# Load up each tab with a dataset
+for dataset_num, tab in enumerate(tab_list):
+    with tab:
+        # Can't get the name of the tab! Can't index key list. So convert to list and index
+        dataset_name = list(datasets.keys())[dataset_num]
+        st.subheader(dataset_name)
+        st.dataframe(datasets[dataset_name],hide_index=True)
+
+# Hide menu and footer
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
---
+++ classes.py
@@ -0,0 +1,73 @@
+#################################################################################
+# Chat2VIS supporting functions
+# https://chat2vis.streamlit.app/
+# Paula Maddigan
+#################################################################################
+
+from langchain import HuggingFaceHub, LLMChain,PromptTemplate
+
+def run_request(question_to_ask, model_type, alt_key):
+    # Hugging Face model
+    llm = HuggingFaceHub(huggingfacehub_api_token = alt_key, repo_id="codellama/" + model_type, model_kwargs={"temperature":0.1, "max_new_tokens":500})
+    llm_prompt = PromptTemplate.from_template(question_to_ask)
+    llm_chain = LLMChain(llm=llm,prompt=llm_prompt)
+    llm_response = llm_chain.predict()
+    # rejig the response
+    llm_response = format_response(llm_response)
+    return llm_response
+
+def format_response(res):
+    # Remove the load_csv from the answer if it exists
+    csv_line = res.find("read_csv")
+    if csv_line > 0:
+        return_before_csv_line = res[0:csv_line].rfind("\n")
+        if return_before_csv_line == -1:
+            # The read_csv line is the first line so there is nothing to need before it
+            res_before = ""
+        else:
+            res_before = res[0:return_before_csv_line]
+        res_after = res[csv_line:]
+        return_after_csv_line = res_after.find("\n")
+        if return_after_csv_line == -1:
+            # The read_csv is the last line
+            res_after = ""
+        else:
+            res_after = res_after[return_after_csv_line:]
+        res = res_before + res_after
+    return res
+
+def format_question(primer_desc,primer_code , question, model_type):
+    # Fill in the model_specific_instructions variable
+    instructions = ""
+    if model_type == "Code Llama":
+        # Code llama tends to misuse the "c" argument when creating scatter plots
+        instructions = "Create a figure object named fig using plotly express. Do not show the fig.\n"
+        instructions += "Pass it to : st.plotly_chart(fig,use_container_width=True)"
+        #instructions += "Do not pass the 'c' argument to the seaborn plot function, use 'color' instead and only pass color names like 'green', 'red', 'blue'."
+    primer_desc = primer_desc.format(instructions)  
+    # Put the question at the end of the description primer within quotes, then add on the code primer.
+    return  '"""\n' + primer_desc + question + '\n"""\n' + primer_code
+
+def get_primer(df_dataset,df_name):
+    # Primer function to take a dataframe and its name
+    # and the name of the columns
+    # and any columns with less than 20 unique values it adds the values to the primer
+    # and horizontal grid lines and labeling
+    primer_desc = "Use a dataframe called df from data_file.csv with columns '" \
+        + "','".join(str(x) for x in df_dataset.columns) + "'. "
+    for i in df_dataset.columns:
+        if len(df_dataset[i].drop_duplicates()) < 20 and df_dataset.dtypes[i]=="O":
+            primer_desc = primer_desc + "\nThe column '" + i + "' has categorical values '" + \
+                "','".join(str(x) for x in df_dataset[i].drop_duplicates()) + "'. "
+        elif df_dataset.dtypes[i]=="int64" or df_dataset.dtypes[i]=="float64":
+            primer_desc = primer_desc + "\nThe column '" + i + "' is type " + str(df_dataset.dtypes[i]) + " and contains numeric values. "   
+    primer_desc = primer_desc + "\nLabel the x and y axes appropriately."
+    primer_desc = primer_desc + "\nAdd a title. Set the fig suptitle as empty."
+    primer_desc = primer_desc + "{}" # Space for additional instructions if needed
+    primer_desc = primer_desc + "\nUsing Python version 3.9.12, create a script using the dataframe df to graph the following: "
+    #pimer_code = "import pandas as pd\nimport seaborn as sns\nimport matplotlib.pyplot as plt\n"
+    pimer_code = "import pandas as pd\nimport plotly.express as px\nimport streamlit as st\nimport matplotlib.pyplot as plt\n"
+    #pimer_code = pimer_code + "fig,ax = plt.subplots(1,1,figsize=(10,4))\n"
+    #pimer_code = pimer_code + "ax.spines['top'].set_visible(False)\nax.spines['right'].set_visible(False) \n"
+    pimer_code = pimer_code + "df=" + df_name + ".copy()\n"
+    return primer_desc,pimer_code
