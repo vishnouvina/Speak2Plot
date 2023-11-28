679e3d5aed47283a082ffa0a883124f583479843
Vishnou Vinayagame <vishnou.vinayagame@student-cs.fr>
2023-11-28 14:45:24 -0500

    generate insights in the chat

--- home.py
+++ home.py
@@ -1,106 +1,119 @@
-import pandas as pd
 import streamlit as st
-
-from classes import get_primer, format_question, run_request
+import pandas as pd
+from transformers import Pix2StructProcessor, Pix2StructForConditionalGeneration
+from helper_functions import get_primer, format_question, run_request, generate_insights
 import warnings
+
+st.set_page_config(
+    page_title="Build a data visualisation bot, powered by Code-Llama and Zephyr-Beta",
+    page_icon="🦙",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
 warnings.filterwarnings("ignore")
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.set_page_config(layout="wide",page_title="Chat2VIS")
 
-st.markdown("<h2 style='text-align: center;padding-top: 0rem;'>Creating Visualisations using Natural Language with Code Llama</h2>", unsafe_allow_html=True)
+def auto_scroll_to_bottom():
+    st.markdown(
+        '<script>window.scrollTo(0,document.body.scrollHeight);</script>', 
+        unsafe_allow_html=True
+    )
+
+st.header("Creating Visualisations using Natural Language with Code Llama 💬")
 
 available_models = {"Code Llama":"codellama/CodeLlama-34b-Instruct-hf", 
-                    "Zephyr Beta":"HuggingFaceH4/zephyr-7b-beta"
+                    #"Zephyr Beta":"HuggingFaceH4/zephyr-7b-beta"
                     }
 
-hf_key = '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'
+hf_key = st.secrets["hf_key"]
 
-# List to hold datasets
+processor = Pix2StructProcessor.from_pretrained('google/deplot')
+model = Pix2StructForConditionalGeneration.from_pretrained('google/deplot')
+
 if "datasets" not in st.session_state:
     datasets = {}
-    # Preload datasets
-    #datasets["Feedbacks"] = clean_dataset(pd.read_csv("data/translated_feedbacks.csv"), available_models["Code Llama"], hf_key)
-    #datasets["History"] = clean_dataset(pd.read_csv("data/history.csv"), available_models["Code Llama"], hf_key)
     datasets["Sleep"] = pd.read_csv("data/physionet_cleaned/sleep.csv", parse_dates=['date'])
     datasets["Screen"] = pd.read_csv("data/physionet_cleaned/screen.csv", parse_dates=['date'])
     datasets["Steps"] = pd.read_csv("data/physionet_cleaned/steps.csv", parse_dates=['date'])
     st.session_state["datasets"] = datasets 
 else:
-    # use the list already loaded
     datasets = st.session_state["datasets"]
 
 with st.sidebar:
-    # First we want to choose the dataset, but we will fill it with choices once we've loaded one
     dataset_container = st.empty()
 
-    # Add facility to upload a dataset
     try:
         uploaded_file = st.file_uploader(":computer: Load a CSV file:", type="csv")
         index_no=0
         if uploaded_file:
-            # Read in the data, add it to the list of available datasets. Give it a nice name.
             file_name = uploaded_file.name[:-4].capitalize()
-            # Clean the chosen dataset
             datasets[file_name] = pd.read_csv(uploaded_file)
-            # We want to default the radio button to the newly added dataset
             index_no = len(datasets)-1
     except Exception as e:
         st.error("File failed to load. Please select a valid CSV file.")
         print("File failed to load.\n" + str(e))
-    # Radio buttons for dataset choice
-    chosen_dataset = dataset_container.radio(":bar_chart: Choose your data:",datasets.keys(),index=index_no)#,horizontal=True,)
+    chosen_dataset = dataset_container.radio(":bar_chart: Choose your data:",datasets.keys(),index=index_no)
 
     selected_model = st.radio(
     ":brain: Choose your model:", available_models.keys(),
     captions = available_models.values())
 
- # Text area for query
-question = st.text_area("What would you like to visualise?",height=10)
-go_btn = st.button("Go...")
 
-# Make a list of the models which have been selected
-selected_models = [selected_model]
-model_count = len(selected_models)
+if "messages" not in st.session_state.keys(): 
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Hello ! How can I help you ?"}
+    ]
 
-# Execute chatbot query
-if go_btn and model_count > 0:
-    # Place for plots depending on how many models
-    #plots = st.tabs(model_count)
-    # Get the primer for this dataset
-    primer1,primer2 = get_primer(datasets[chosen_dataset],'datasets["'+ chosen_dataset + '"]') 
-    # Create model, run the request and print the results
-    for plot_num, model_type in enumerate(selected_models):
-        #with plots[plot_num]:
-        with st.container():
-            st.subheader(model_type)
+if prompt := st.chat_input("Your question"): 
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: 
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+primer1, primer2 = get_primer(datasets[chosen_dataset],'datasets["'+ chosen_dataset + '"]') 
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
             try:
-                # Format the question 
-                question_to_ask = format_question(primer1, primer2, question, model_type)   
-                # Run the question
+                fig = None
+                question_to_ask = format_question(primer1, primer2, prompt)   
                 answer=""
-                answer = run_request(question_to_ask, available_models[model_type], alt_key=hf_key)
-                # the answer is the completed Python script so add to the beginning of the script to it.
+                answer = run_request(question_to_ask, available_models[selected_model], alt_key=hf_key)
                 answer = primer2 + answer
-                print("Model: " + model_type)
+                print("Model: " + selected_model)
+
                 print(answer)
-                #plot_area = st.empty()
-                #plot_area.pyplot(exec(answer))
-                exec(answer)           
+                exec(answer)
+
+                insights = generate_insights(processor, model, fig, "HuggingFaceH4/zephyr-7b-beta", hf_key)
+                print(insights)
+                st.write(insights)
+
+                message = {"role": "assistant", "content": fig}
+            
             except Exception as e:
                 print(e)
-                st.error("Unfortunately the code generated from the model contained errors and was unable to execute.")
+                message = {"role": "assistant", "content": "Unfortunately the code generated from the model contained errors and was unable to execute."}
+                st.write(message['content'])
 
-# Display the datasets in a list of tabs
-# Create the tabs
-tab_list = st.tabs(datasets.keys())
+            st.session_state.messages.append(message) 
 
-# Load up each tab with a dataset
-for dataset_num, tab in enumerate(tab_list):
-    with tab:
-        # Can't get the name of the tab! Can't index key list. So convert to list and index
-        dataset_name = list(datasets.keys())[dataset_num]
-        st.subheader(dataset_name)
-        st.dataframe(datasets[dataset_name],hide_index=True)
+auto_scroll_to_bottom()
+
+def tabs_datasets():
+    tab_list = st.tabs(datasets.keys())
+
+    # Load up each tab with a dataset
+    for dataset_num, tab in enumerate(tab_list):
+        with tab:
+            # Can't get the name of the tab! Can't index key list. So convert to list and index
+            dataset_name = list(datasets.keys())[dataset_num]
+            st.subheader(dataset_name)
+            st.dataframe(datasets[dataset_name],hide_index=True)
 
 # Hide menu and footer
 hide_streamlit_style = """
